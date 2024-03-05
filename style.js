f94971f4548847ab29b2cb664995a36ce12b704a
Edgar Hernandez <edgar.hdz.glez7@gmail.com>
2024-03-05 17:55:01 -0600

    Estilos agregados

--- style.js
+++ style.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('cambiarNombreBtn').addEventListener('click', function() {
+        let nombre = prompt("¿Cuál es tu nombre?");
+        if (nombre != null) {
+            document.getElementById("saludo").textContent = "¡Hola " + nombre + "!";
+        }
+    });
+});
+
 
